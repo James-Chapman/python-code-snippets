d0689e2e48c2de695f170479e238639c2d3760ff
James-Chapman <github@linux-101.org>
2019-02-13 20:38:04 +0000

    Added file serving and request routing to the web server and then tested it with parameterized.

--- HTTP/header_printing_web_server.py
+++ HTTP/header_printing_web_server.py
@@ -16,18 +16,78 @@ class PythonHttpHandler( http.server.BaseHTTPRequestHandler ):
     def do_GET( self ):
         self.log_message( "Command: %s Path: %s Headers: %r"
                           % ( self.command, self.path, self.headers.items() ) )
-        self.send_response(200, 'OK')
-        self.send_header('Content-type', 'html')
-        self.end_headers()
-        self.wfile.write(bytes("<html> <head><title> do_GET </title> </head> <body> </body> </html>", 'UTF-8'))
+        self.getPathRouter()
 
     def do_POST( self ):
         self.log_message( "Command: %s Path: %s Headers: %r"
                           % ( self.command, self.path, self.headers.items() ) )
-        self.send_response(200, 'OK')
-        self.send_header('Content-type', 'html')
+        self.postPathRouter()
+
+    def sendResponse(self, code, reason, responseString):
+        self.send_response(code, reason)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-length', len(responseString))
         self.end_headers()
-        self.wfile.write(bytes("<html> <head><title> do_POST </title> </head> <body> </body> </html>", 'UTF-8'))
+        self.wfile.write(responseString)
+
+    def getPathRouter(self):
+        if (self.path == '/'):
+            responseString = bytes("<html><head><title>do_GET</title></head><body><h1>do_GET</h1></body></html>",
+                                   'UTF-8')
+            self.sendResponse(200, 'OK', responseString)
+            return
+
+        if (self.path == "/favicon.ico"):
+            self.sendResponse(404, 'Not found', b'')
+            return
+
+        path = self.path.split('/')
+        route = str(path[1])
+        if (route == "download"):
+            print("%s" % path[2])
+            self.handleDownload("/".join(path[2:]))
+        else:
+            responseString = bytes(
+                "<html><head><title>404</title></head><body><h1>404</h1></body></html>",
+                'UTF-8')
+            self.sendResponse(404, 'Not found', responseString)
+
+    def postPathRouter(self):
+        if (self.path == '/'):
+            responseString = bytes("<html><head><title>do_POST</title></head><body><h1>do_POST</h1></body></html>",
+                                   'UTF-8')
+            self.sendResponse(200, 'OK', responseString)
+            return
+        else:
+            responseString = bytes(
+                "<html><head><title>404</title></head><body><h1>404</h1></body></html>",
+                'UTF-8')
+            self.sendResponse(404, 'Not found', responseString)
+
+    def handleDownload(self, filePath):
+        # TODO: Check for '..' chars that try to escape download path
+        print(filePath)
+        with open(filePath, "rb") as f:
+            f.seek(0, 2)
+            size = f.tell()
+            f.seek(0, 0)
+            chunkSize = size
+            remainSize = size
+            if (size > 0x200):
+                chunkSize = 0x200
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'application/octet-stream')
+            self.send_header('Content-Length', size)
+            self.send_header('Content-Disposition', 'attachment; filename="%s"' % filePath)
+            self.end_headers()
+            while (remainSize > 0):
+                if (remainSize < chunkSize):
+                    chunkSize = remainSize
+                chunk = f.read(chunkSize)
+                self.wfile.write(chunk)
+                remainSize -= chunkSize
+
+
 
 
 def httpd(handler_class=PythonHttpHandler, server_address = ('', 8008), ):
--- HTTP/testPythonHttpHandler.py
+++ HTTP/testPythonHttpHandler.py
@@ -4,9 +4,10 @@ import threading
 from header_printing_web_server import PythonHttpHandler
 import pytest
 import http
+from parameterized import parameterized, parameterized_class
 
 def httpd(handler_class=PythonHttpHandler, server_address=('127.0.0.1', 8008), ):
-    for i in range(2):
+    for i in range(7):
         server = http.server.HTTPServer(server_address, handler_class)
         server.handle_request()  # serve_forever
 
@@ -33,7 +34,7 @@ class TestPythonHttpHandler:
             r1 = h1.getresponse()
             assert(r1.status == 200)
             assert(r1.reason == 'OK')
-            assert(r1.read(200) == b'<html> <head><title> do_GET </title> </head> <body> </body> </html>')
+            assert(r1.read(200) == b'<html><head><title>do_GET</title></head><body><h1>do_GET</h1></body></html>')
 
     [pytest]
     def test_do_POST(self):
@@ -43,5 +44,21 @@ class TestPythonHttpHandler:
             r1 = h1.getresponse()
             assert(r1.status == 200)
             assert(r1.reason == 'OK')
-            assert(r1.read(200) == b'<html> <head><title> do_POST </title> </head> <body> </body> </html>')
+            assert(r1.read(200) == b'<html><head><title>do_POST</title></head><body><h1>do_POST</h1></body></html>')
 
+    # Parameterized makes testing easy...
+    # Specify variations and expected results. This isn't really testing single functions though
+    @parameterized.expand([
+        ("GET", "/", 200, 'OK', b'<html><head><title>do_GET</title></head><body><h1>do_GET</h1></body></html>'),
+        ("GET", "/doesntexist", 404, 'Not found', b'<html><head><title>404</title></head><body><h1>404</h1></body></html>'),
+        ("GET", "/favicon.ico", 404, 'Not found', b''),
+        ("POST", "/", 200, 'OK', b'<html><head><title>do_POST</title></head><body><h1>do_POST</h1></body></html>'),
+        ("POST", "/doesntexist", 404, 'Not found', b'<html><head><title>404</title></head><body><h1>404</h1></body></html>'),
+    ])
+    def test_Request(self, request, path, code, reason, expected):
+        h1 = http.client.HTTPConnection('127.0.0.1:8008')
+        h1.request(request, path)
+        r1 = h1.getresponse()
+        assert(r1.status == code)
+        assert(r1.reason == reason)
+        assert(r1.read(200) == expected)
